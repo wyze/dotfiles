b1a26b90f204a749384e467d70bd9fbf7a069e7a
Neil Kistner <neil.kistner@gmail.com>
2015-08-28 21:38:34 -0500

    Add git clone to install script
    Will now clone itself and run the installation.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Cloning dotfiles"
+git clone --depth=1 https://github.com/wyze/dotfiles.git "$HOME/.dotfiles"
+cd "$HOME/.dotfiles"
+
 echo "Installing dotfiles"
 
 if [ "$(uname)" == "Darwin" ]; then
