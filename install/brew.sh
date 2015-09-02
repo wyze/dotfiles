b308347d4f88474db434f8eb306ee55ada2aeb51
Neil Kistner <neil.kistner@gmail.com>
2015-09-01 22:29:08 -0500

    Update Homebrew installation
    - Update Homebrew.
    - Upgrade existing formulae.
    - Add htop package.
    - Use correct option to replace Vim.
    - Do some cleanup when done.

--- install/brew.sh
+++ install/brew.sh
@@ -5,17 +5,27 @@ if test ! $(which brew); then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+echo "Updating Homebrew"
+brew update
+
+echo "Upgrade installed formulae"
+brew upgrade --all
+
 echo "Installing Homebrew packages"
 
 # cli tools
+brew install htop
 brew install wget
 
 # development tools
 brew install git
-brew install vim --override-system-vim
+brew install vim --override-system-vi
 brew install reattach-to-user-namespace
 brew install tmux
 brew install zsh
 brew install highlight
 brew install nvm
 brew install z
+
+# Remove outdated versions from the cellar
+brew cleanup
