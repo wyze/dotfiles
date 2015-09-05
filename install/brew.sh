bd12b5b615cc1f2bc5e843d4e790795961fa8faa
Neil Kistner <neil.kistner@gmail.com>
2015-09-04 21:18:19 -0500

    Install Docker
    - Install Docker from Homebrew.

--- install/brew.sh
+++ install/brew.sh
@@ -13,11 +13,17 @@ brew upgrade --all
 
 echo "Installing Homebrew packages"
 
+# cask it
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
 # cli tools
 brew install htop
 brew install wget
 
 # development tools
+brew install boot2docker
+brew install docker
 brew install git
 brew install vim --override-system-vi
 brew install reattach-to-user-namespace
@@ -26,6 +32,7 @@ brew install zsh
 brew install highlight
 brew install nvm
 brew install z
+brew cask install virtualbox
 
 # Remove outdated versions from the cellar
 brew cleanup
